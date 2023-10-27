16328a6151e2fb40327e1d17c0a36e4ac11c2c24
spioneracorei8 <bestza147.aa@gmail.com>
2023-10-27 10:48:11 +0700

    feat: created api get balance data of total revenue and total expense

--- server/apps/revenueExpense.js
+++ server/apps/revenueExpense.js
@@ -4,11 +4,32 @@ import mongoose from "mongoose";
 
 const revenueExpenseRouter = Router()
 
+// Get all revenue expense and balance data
 revenueExpenseRouter.get("/", async (req, res) => {
     try {
-        const revenueExpenseData = await RevenueExpense.find({})
+        const revenueExpenseAllData = await RevenueExpense.find({})
+        const balanceRevenueExpense = await RevenueExpense.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    totalRevenue: { $sum: "$revenues" },
+                    totalExpense: { $sum: "$expenses" }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    totalRevenue: 1,
+                    totalExpense: 1,
+                    balance: { $subtract: ["$totalRevenue", "$totalExpense"] }
+                }
+            }
+        ])
+
+        let revenueExpenseBalanceData = [...revenueExpenseAllData, ...balanceRevenueExpense]
+
         return res.status(200).json({
-            "data": revenueExpenseData
+            "data": revenueExpenseBalanceData
         })
     } catch (error) {
         return res.status(500).json({
@@ -18,7 +39,37 @@ revenueExpenseRouter.get("/", async (req, res) => {
     }
 })
 
+// Get balance data
+revenueExpenseRouter.get("/balance", async (req, res) => {
 
+    try {
+        const balanceRevenueExpense = await RevenueExpense.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    totalRevenue: { $sum: "$revenues" },
+                    totalExpense: { $sum: "$expenses" }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    totalRevenue: 1,
+                    totalExpense: 1,
+                    balance: { $subtract: ["$totalRevenue", "$totalExpense"] }
+                }
+            }
+        ])
+        return res.status(200).json(balanceRevenueExpense[0])
+    } catch (error) {
+        return res.status(500).json({
+            "message": "An error occurred while fetching data",
+            "error": error
+        })
+    }
+})
+
+// Add new revenue and expense
 revenueExpenseRouter.post("/", async (req, res) => {
     const { description, revenues, expenses } = req.body
 
@@ -42,7 +93,7 @@ revenueExpenseRouter.post("/", async (req, res) => {
     }
 })
 
-
+// Delete revenue and expense by _id
 revenueExpenseRouter.delete("/:id", async (req, res) => {
     const revenueExpenseId = new mongoose.Types.ObjectId(req.params.id)
     try {
