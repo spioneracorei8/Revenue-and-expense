a30ea8a33be6170c6eb6560a517c4187f2340a70
spioneracorei8 <bestza147.aa@gmail.com>
2023-11-05 14:50:04 +0700

    feat: created api get revenue-expense history and api delete revenue-expense history

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import HomePage from './pages/homePage'
 import RevenueExpenseFormPage from './pages/revenueExpenseFormPage'
+import RevenueExpenseRemovePage from './pages/revenueExpenseRemovePage'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/form-revenue-expense' element={<RevenueExpenseFormPage />} />
+        <Route path='/remove-revenue-expense' element={<RevenueExpenseRemovePage />} />
       </Routes>
     </>
   )
---
+++ client/src/pages/revenueExpenseRemovePage.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import Navigationbar from '../components/navigationbar'
+import "../style/revenueExpenseRemovePage.css"
+import useRevenueExpnese from '../hook/useRevenueExpense'
+
+const RevenueExpenseRemovePage = () => {
+
+    const { revenueExpenseData, setRevenueExpenseData, removeRevenueExpenseHistory } = useRevenueExpnese()
+
+    const handleRemoveRevenueExpense = (id) => {
+        removeRevenueExpenseHistory(id)
+        const newRevenueExpenseData = revenueExpenseData.filter((item) => {
+            return item._id != id
+        })
+        setRevenueExpenseData(newRevenueExpenseData)
+    }
+
+    return (
+        <>
+            <Navigationbar />
+            <div className='container-revenue-expense-remove'>
+                {
+                    revenueExpenseData.map((item) => {
+                        return (
+                            <div>
+                                <p>{item.description}</p>
+                                <button onClick={() => handleRemoveRevenueExpense(item._id)}>Delete</button>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
+
+export default RevenueExpenseRemovePage
