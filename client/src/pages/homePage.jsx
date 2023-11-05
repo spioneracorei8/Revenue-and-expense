4db8aea491fd8eb7100ebf55f35666d30aa1e42d
spioneracorei8 <bestza147.aa@gmail.com>
2023-11-05 15:14:53 +0700

    feat: map revenue-expense data and api post revenue-expense

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -1,11 +1,29 @@
 
 import React from 'react'
 import Navigationbar from '../components/navigationbar'
+import useRevenueExpnese from '../hook/useRevenueExpense'
+import "../style/homePage.css"
 
 const HomePage = () => {
+    const { revenueExpenseData } = useRevenueExpnese()
+    console.log(revenueExpenseData);
     return (
         <>
             <Navigationbar />
+            <div className='container-revenue-expense-data'>
+                {
+                    revenueExpenseData.map((item) => {
+                        return (
+                            <div className='revenue-expense-data'>
+                                <h1>{item.description}</h1>
+                                <h2>Type of money: {item.type}</h2>
+                                <h3>{item.amount}</h3>
+                                <h4>{item.date}</h4>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
